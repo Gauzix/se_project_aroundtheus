3fa38798de3e1845617f9c8ddc6dae1799c483df
Rob Kelley <robinsonkells@gmail.com>
2024-10-30 01:10:06 -0600

    Adding JS for gallery cards. Refactoring index.js

--- scripts/index.js
+++ scripts/index.js
@@ -25,20 +25,67 @@ const initialCards =[
     }
 ];
 
+/* -------------------------------------------------------------------------- */
+/*                                  Elements                                  */
+/* -------------------------------------------------------------------------- */
 const profileEditModal = document.querySelector('#profile-edit-modal');
-const profileEditClose = document.querySelector('#profile-edit-close');
+const profileEditForm = profileEditModal.querySelector('.modal__form');
+const profileEditClose = profileEditModal.querySelector('.modal__close');
 const profileEditBtn = document.querySelector('#profile-edit-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
+const cardListEl = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
 
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+function closePopup(){
+    profileEditModal.classList.remove('modal_opened');
+}
+
+function getCardElement(cardData) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector('.card__image');
+    const cardTitleEl = cardElement.querySelector('.card__title');
+
+    /* set the path to the image to the link field of the object
+set the image alt text to the name field of the object
+set the card title to the name field of the object, too
+    cardTitleEl.textContent = cardData.name; */
+    cardImageEl.src = cardData.link;
+    cardImageEl.alt = cardData.name;
+    cardTitleEl.textContent = cardData.name;
+    return cardElement;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Event Handlers                               */
+/* -------------------------------------------------------------------------- */
+function handleProfileEditSubmit (evt){
+    evt.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+}
+
+/* -------------------------------------------------------------------------- */
+/*                              Event Listeners                              */
+/* -------------------------------------------------------------------------- */
 profileEditBtn.addEventListener('click', () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add('modal_opened');
 }
 );
 
-profileEditClose.addEventListener('click', () => {
-    profileEditModal.classList.remove('modal_opened');
-}
-);
+profileEditClose.addEventListener('click', closePopup);
 
+profileEditForm.addEventListener('submit', handleProfileEditSubmit);
 
-
-
+initialCards.forEach((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+});
